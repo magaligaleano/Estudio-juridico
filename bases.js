40bfd8f1fdc8cdfafd22156850c6a8704bf9994e
Magali Galeano <91576082+magaligaleano@users.noreply.github.com>
2022-10-11 10:10:07 -0300

    Add files via upload

---
+++ bases.js
@@ -0,0 +1,27 @@
+console.log("Hola");
+
+let nombre = "Juan";
+let edad = 40;
+let casado = false;
+
+let pasaTiempos = ["tenis", "futbol", "runing", 20, -50];
+
+let compras = {
+  supermercado: "desodorantes",
+  verduleria: "tomates",
+  despensa: "pan",
+  total: 500,
+};
+// if (edad !== 40) {
+//   alert('No tiene 40');
+// } else {
+//   alert('Tiene 40 años');
+//}
+
+if (edad !== 40) {
+  console.log("No tiene 40");
+} else {
+  console.log("Tiene 40 años");
+}
+
+console.log(compras);
